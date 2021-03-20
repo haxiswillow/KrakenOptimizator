8d03ab784914cf997647938da23ecafb5b3609bc
haxiswillow <78441034+haxiswillow@users.noreply.github.com>
2021-03-20 13:10:32 -0300

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,3 +2,15 @@
 # GitHub @haxiswillow
 #!/system/bin/sh
 MODDIR=${0%/*}
+
+rm /data/cache/*
+rm /data/local/*.apk
+rm /cache/*
+rm /data/log/*.log
+rm /data/local/tmp/*.apk
+rm /cache/recovery/*
+rm /data/*.log
+rm /data/backup/pending/*.tmp
+rm /data/tombstones/*
+rm /sdcard/LOST.DIR/*
+exit 0;
