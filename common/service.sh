bbdbb65606fdc4a804f132dfe285f348a7068973
haxiswillow <78441034+haxiswillow@users.noreply.github.com>
2021-03-22 11:26:53 -0300

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -8,7 +8,7 @@ echo "4096" > /proc/sys/kernel/random/read_wakeup_threshold
 echo "4096" > /proc/sys/kernel/random/write_wakeup_threshold
 
 # Suspend Power
-write /sys/kernel/power_suspend/ "3"
+echo "3" /sys/kernel/power_suspend
 
 # Charge
 echo "1" /sys/kernel/fast_charge/force_fast_charge
@@ -43,5 +43,14 @@ echo "performance" /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 # For Battery
 echo "0" /sys/touchpanel/double_tap
 echo "Y" /sys/module/worqueue/parameters/power_efficient
+
+# Low Memory Killer Parameters
+echo "0,3,5,7,14,15" > /sys/module/lowmemorykiller/parameters/adj
+echo "18432,23040,27648,32256,36864,46080" > /sys/module/lowmemorykiller/parameters/minfree
+echo "0" > /proc/sys/kernel/panic
+echo "0" > /proc/sys/vm/oom_kill_allocating_task
+echo "0" > /proc/sys/vm/panic_on_oom
+echo "1" > /proc/sys/kernel/panic_on_oops
+echo "0" > /proc/sys/kernel/panic
 
 exit 0;
